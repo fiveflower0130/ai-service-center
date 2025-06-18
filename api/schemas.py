82b214f54d21d0deae0c834c86a406766b1628e7
Dante <Dante_Chen@aseglobal.com>
2025-06-18 17:19:16 +0800

    feat(api): 新增 Resp 類別以擴展回應結構

--- api/schemas.py
+++ api/schemas.py
@@ -1,4 +1,10 @@
 from pydantic import BaseModel
+from typing import Optional,Any
+
+class Resp(BaseModel):
+    code: str
+    error: str
+    data: Any
 
 class ClassificationRequest(BaseModel):
     img_src: str
