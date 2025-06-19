65e797c1415e09313f0e10d19dd0ddb008a8894c
Dante <Dante_Chen@aseglobal.com>
2025-06-19 11:32:15 +0800

    fix(main): 更新應用程式路徑以反映目錄結構變更

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
 import uvicorn
-from api.app import app  
 
 if __name__ == "__main__":
-    uvicorn.run(app="api.app:app", host="0.0.0.0", port=8009, reload=True)
+    uvicorn.run(app="app.app:app", host="0.0.0.0", port=8009, reload=True)
