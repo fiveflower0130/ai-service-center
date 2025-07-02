c6e7f53de872ebac40ed4aa7a669f0d8e917f7ac
Dante <Dante_Chen@aseglobal.com>
2025-07-02 17:31:42 +0800

    feat(models): 新增 MySQL 數據模型及初始化文件

---
+++ app/models/mysql_model.py
@@ -0,0 +1,33 @@
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer, String, DateTime, Float
+from  app.database.mysql_database import mysql_base
+
+class DrillMapAIPredictionRecord(mysql_base):
+    __tablename__ = "prediction_record"
+    __table_args__ = {
+        "mysql_engine": "InnoDB",
+        "mysql_charset": "utf8mb4",
+        "mysql_collate": "utf8mb4_general_ci",
+        "mysql_row_format": "DYNAMIC"
+    }
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    image_path = Column(String(256), index=True)
+    product_name = Column(String(64))
+    classification_code = Column(String(8))
+    classification_model = Column(String(32))
+    mahalanobis_distance = Column(Float)
+    classification_time = Column(DateTime)
+
+class DrillMapAIPredictionCode(mysql_base):
+    __tablename__ = "classification_code"
+    __table_args__ = {
+        "mysql_engine": "InnoDB",
+        "mysql_charset": "utf8mb4",
+        "mysql_collate": "utf8mb4_general_ci",
+        "mysql_row_format": "DYNAMIC"
+    }
+
+    code = Column(String(8), primary_key=True, index=True)
+    directions = Column(String(64), index=True)
+    
