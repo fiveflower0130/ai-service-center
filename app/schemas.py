32db29d8287e4721d449e426b2f692fcf0a6d12a
Dante <Dante_Chen@aseglobal.com>
2025-07-02 15:22:11 +0800

    feat(schemas): 新增分類請求及回應的數據模型

---
+++ app/schemas.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from pydantic import BaseModel
+from typing import Optional, Any
+
+class Resp(BaseModel):
+    code: str
+    error: str
+    data: Any
+
+class ClassificationRequest(BaseModel):
+    img_src: str
+    product_name: str
+
+class ClassificationRecord(BaseModel):
+    image_path: str
+    product_name: str
+    classification_code: str
+    classification_model: str
+    mahalanobis_distance: float
+    classification_time: datetime
